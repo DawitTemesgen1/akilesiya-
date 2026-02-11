edbd9995668203f4013a1026f874bac7689f14d5
Dawit Temesgen <dawittemesgen@gmail.com>
2026-02-11 14:37:41 +0300

    fix: Add missing foundation import for debugPrint

--- lib/services/template_service.dart
+++ lib/services/template_service.dart
@@ -1,6 +1,7 @@
 // lib/services/template_service.dart
 
 import 'dart:convert';
+import 'package:flutter/foundation.dart';
 import 'package:amde_haymanot_abalat_guday/services/api_service.dart';
 import 'package:http/http.dart' as http;
 
