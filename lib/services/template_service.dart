9c0b6b7d62dd52b00ee5fb71eb2a5c16c471fe10
Dawit Temesgen <dawittemesgen@gmail.com>
2026-02-11 14:38:56 +0300

    refactor: Clean up debug logs and unused imports

--- lib/services/template_service.dart
+++ lib/services/template_service.dart
@@ -1,7 +1,6 @@
 // lib/services/template_service.dart
 
 import 'dart:convert';
-import 'package:flutter/foundation.dart';
 import 'package:amde_haymanot_abalat_guday/services/api_service.dart';
 import 'package:http/http.dart' as http;
 
@@ -28,15 +27,13 @@ class TemplateService {
 
   static Future<Map<String, dynamic>> createCustomField(
       String name, String managedBy, String profileTab, String fieldType) {
-    final body = {
+    return _handleResponse(ApiService.post('/template/fields', {
       'name': name,
       'managed_by': managedBy,
       'profile_tab': profileTab,
       'field_type': fieldType,
       'type': fieldType,
-    };
-    debugPrint('DEBUG: TemplateService.createCustomField body: $body');
-    return _handleResponse(ApiService.post('/template/fields', body));
+    }));
   }
 
   static Future<Map<String, dynamic>> deleteCustomField(int id) {
@@ -45,15 +42,13 @@ class TemplateService {
 
   static Future<Map<String, dynamic>> updateCustomField(int id, String name,
       String managedBy, String profileTab, String fieldType) {
-    final body = {
+    return _handleResponse(ApiService.put('/template/fields/$id', {
       'name': name,
       'managed_by': managedBy,
       'profile_tab': profileTab,
       'field_type': fieldType,
       'type': fieldType,
-    };
-    debugPrint('DEBUG: TemplateService.updateCustomField body: $body');
-    return _handleResponse(ApiService.put('/template/fields/$id', body));
+    }));
   }
 
   // === Option Functions ===
